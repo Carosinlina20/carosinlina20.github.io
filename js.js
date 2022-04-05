75f9b185862a92cac08ff7ef56cc3f32a2d18499
Carol <87648527+Carosinlina20@users.noreply.github.com>
2022-04-05 10:13:42 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+
+   $(".nav-item").click(function (){
+       $(".nav-item").removeClass("active");
+
+       $(".nav-item").children(".line").hide();
+
+       $(this).addClass("active");
+
+       $(this).find(".line").show();
+   });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
